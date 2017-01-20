54f127a20cfe3498d28d6173fae8a3e61f43524c
1170197998 <1170197998@qq.com>
2017-01-20 15:40:27 +0800

    init

---
+++ ZTAdditions/NSString+ZTAddition.h
@@ -0,0 +1,23 @@
+//
+//  NSString+ZTAddition.h
+//  Additions
+//
+//  Created by ShaoFeng on 2017/1/16.
+//  Copyright © 2017年 ShaoFeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NSString (ZTAddition)
+
+/**
+ 返回Text实际size
+
+ @param size    给定Text范围
+ @param label   当前label
+ @param space   行间距
+ @return        对应实际size
+ */
+- (CGSize)calculateRectWithSize:(CGSize)size andLabel:(UILabel *)label lineSpacing:(CGFloat)space;
+
+@end
